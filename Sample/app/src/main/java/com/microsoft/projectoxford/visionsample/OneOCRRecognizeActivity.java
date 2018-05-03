a27ad8a649de203eba9c8a7ec09f146b8c689abf
FAREAST\boywu <boywu@microsoft.com>
2018-05-03 13:38:28 +0800

    update menu for OneOCR v2.0

--- Sample/app/src/main/java/com/microsoft/projectoxford/visionsample/OneOCRRecognizeActivity.java
+++ Sample/app/src/main/java/com/microsoft/projectoxford/visionsample/OneOCRRecognizeActivity.java
@@ -133,7 +133,7 @@ public class OneOCRRecognizeActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_recognize, menu);
+        getMenuInflater().inflate(R.menu.menu_recognize_oneocr, menu);
         return true;
     }
 
